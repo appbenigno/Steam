795e97fc74723b2ea858e0cc0459c0389b661644
ALFRED PHILIP BENIGNO <appbenigno@live.com.ph>
2018-05-13 05:08:11 +0800

    reupload

---
+++ Fishing Planet Assistor/GlobalHotkey/KeyboardHotkey.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace GlobalHotkey
+{
+    public class KeyboardHotkey
+    {
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr windowProcess, int processID, int keyModifier, int keyShortcut);
+
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr windowProcess, int processID);
+    }
+}
---
+++ Fishing Planet Assistor/WindowHook/WinHook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace WindowHook
+{
+    public class WinHook
+    {
+        // Get a handle to an application window.
+        [DllImport("USER32.DLL", CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        // Activate an application window.
+        [DllImport("USER32.DLL")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+    }
+}
